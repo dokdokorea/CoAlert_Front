c14ed9f12b4750e164f82777ef07e3b96df7c5d3
DESKTOP-O257RHD\Youngwoo <duddn14758@gmail.com>
2018-09-09 18:45:30 +0900

    add loading page

---
+++ app/src/main/java/com/example/user/coalert/Loading1Activity.java
@@ -0,0 +1,37 @@
+package com.example.user.coalert;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Loading1Activity extends AppCompatActivity {
+    private ImageView loadingImg;
+    private Animation anim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading1);
+        initView();
+
+        Handler handler=new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                startActivity(new Intent(Loading1Activity.this,MainPage.class));
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0,3000);
+
+    }
+    private void initView(){
+        loadingImg = (ImageView) findViewById(R.id.icon);
+        anim = AnimationUtils.loadAnimation(this, R.anim.loading);
+        anim.setInterpolator(this,android.R.anim.decelerate_interpolator);
+        loadingImg.setAnimation(anim);
+    }
+}
--- app/src/main/java/com/example/user/coalert/LoginActivity.java
+++ app/src/main/java/com/example/user/coalert/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.example.user.coalert;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.facebook.login.Login;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    ImageButton kakao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        kakao=findViewById(R.id.loginbtn);
+
+        kakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,Loading1Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
+
 }
