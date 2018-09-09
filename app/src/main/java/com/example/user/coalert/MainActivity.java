04e7643247c6c5e915bd9bc8eb8ddddedfdf9d5a
unknown <dl57934@naver.com>
2018-09-10 06:43:07 +0900

    complete study Navigation Bar

--- app/src/main/java/com/example/user/coalert/MainActivity.java
+++ app/src/main/java/com/example/user/coalert/MainActivity.java
@@ -2,56 +2,62 @@ package com.example.user.coalert;
 
 import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.example.user.coalert.Fragment.HomeFragment;
-import com.example.user.coalert.Fragment.TimeLineFragment;
-import com.example.user.coalert.Fragment.SearchFragment;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.aurelhubert.ahbottomnavigation.notification.AHNotification;
+
 
 public class MainActivity extends AppCompatActivity {
-    public static final int SERACHID = 2131296279;
-    public static final int HOMEID = 2131296282;
-    public static final int TIMELINEID = 2131296281;
+    public static final int SERACHID = 0;
+    public static final int HOMEID = 1;
+    public static final int TIMELINEID = 2;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        AHBottomNavigation bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        AHBottomNavigationItem searchItem = new AHBottomNavigationItem(R.string.search, android.R.drawable.ic_menu_search, R.color.whiteBackground);
+        AHBottomNavigationItem homeItem = new AHBottomNavigationItem(R.string.home, R.drawable.home, R.color.whiteBackground);
+        AHBottomNavigationItem timeLineItem = new AHBottomNavigationItem(R.string.timeline, android.R.drawable.ic_menu_upload_you_tube, R.color.whiteBackground);
+
+        bottomNavigation.addItem(searchItem);
+        bottomNavigation.addItem(homeItem);
+        bottomNavigation.addItem(timeLineItem);
+
+
+        bottomNavigation.setBehaviorTranslationEnabled(true);
+        bottomNavigation.setAccentColor(Color.parseColor("#51032d"));
+        bottomNavigation.setInactiveColor(R.color.bottom_navigation_select);
+
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                Fragment fragment = new SearchFragment();
-                Log.e("add ", String.valueOf(id));
-                    if(id == R.id.action_one) {
-                        Toast.makeText(MainActivity.this, "Action first Clicked", Toast.LENGTH_LONG).show();
-                        fragment = new SearchFragment();
-                    }
-                   else if (id == R.id.action_two) {
-                        Toast.makeText(MainActivity.this, "Action second Clicked", Toast.LENGTH_LONG).show();
-                        fragment = new HomeFragment();
-                    }
-                    else if( R.id.action_three == id) {
-                        Toast.makeText(MainActivity.this, "Action third Clicked", Toast.LENGTH_LONG).show();
-                        fragment = new TimeLineFragment();
-                    }
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                if (position == 0 ){
 
-                    @SuppressLint("CommitTransaction") FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    ft.replace(R.id.content_fragment_layout, fragment);
-                    ft.commit();
+                }else if (position == 1){
 
+                } else if (position == 2){
+
+                }
                 return true;
             }
         });
+
     }
-
-
 }
