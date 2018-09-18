53feddb76679a132ad21dab12d49f3ae86b46e5a
yeyoung_desktop <7dpdud@naver.com>
2018-09-18 22:30:10 +0900

    making left_navigation_content

---
+++ app/src/main/java/com/example/user/coalert/Activity/LeftNavigationContentActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.coalert.Activity;
+
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.example.user.coalert.R;
+
+public class LeftNavigationContentActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.left_navigation_content);
+
+
+    }
+}
