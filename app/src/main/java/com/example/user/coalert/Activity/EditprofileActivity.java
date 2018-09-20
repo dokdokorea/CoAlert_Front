23e321ba69e4acb882986ccf23c66c463fbee32b
Youngwoo <duddn14758@naver.com>
2018-09-21 04:43:06 +0900

    add edit_profile page

---
+++ app/src/main/java/com/example/user/coalert/Activity/EditprofileActivity.java
@@ -0,0 +1,14 @@
+package com.example.user.coalert.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.user.coalert.R;
+
+public class EditprofileActivity extends AppCompatActivity{
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+}
