3a7ad1bf41ae3c0cd24479ef34f5cb1cbb82929c
Youngwoo <duddn14758@naver.com>
2018-09-21 04:55:46 +0900

    link spinner at edit_profile

--- app/src/main/java/com/example/user/coalert/Activity/EditprofileActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/EditprofileActivity.java
@@ -3,6 +3,9 @@ package com.example.user.coalert.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.user.coalert.R;
 
@@ -10,5 +13,13 @@ public class EditprofileActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
+
+        EditText name=findViewById(R.id.name);
+        Spinner sex=findViewById(R.id.sex);
+        Spinner year=findViewById(R.id.year);
+        Spinner month=findViewById(R.id.month);
+        Spinner day=findViewById(R.id.day);
+        Button broke=findViewById(R.id.broke);
+        Button accept=findViewById(R.id.editbtn);
     }
 }
--- app/src/main/java/com/example/user/coalert/Activity/MypageActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/MypageActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -25,11 +26,13 @@ import java.util.List;
 
 public class MypageActivity extends AppCompatActivity{
     List<String> list;
+    Button Edit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
+        Edit=(Button)findViewById(R.id.edit_personal_info);
         RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recyclerview);
         LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         recyclerView.setHasFixedSize(true);
