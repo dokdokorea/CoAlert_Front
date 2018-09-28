5143c144af0d7fd2567405e8343488b9a17566ab
unknown <dl57934@naver.com>
2018-09-29 02:26:26 +0900

    add retrofit

---
+++ app/src/main/java/com/example/user/coalert/forRestServer/loginModel.java
@@ -0,0 +1,22 @@
+package com.example.user.coalert.forRestServer;
+
+public class loginModel {
+    String id;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
