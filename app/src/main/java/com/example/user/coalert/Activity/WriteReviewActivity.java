08ea35727f7f38ea94aa0cc1f87b57f669414f0c
dokdokorea <7dpdud@naver.com>
2018-09-30 07:05:54 +0900

    in the process of building the write_review layout

--- app/src/main/java/com/example/user/coalert/Activity/WriteReviewActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/WriteReviewActivity.java
@@ -3,14 +3,20 @@ package com.example.user.coalert.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.EditText;
 
 import com.example.user.coalert.R;
 
 public class WriteReviewActivity extends AppCompatActivity {
-
+    private final int MaxLengthOfOneLineContent=100;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_review);
+   EditText editText=(EditText)findViewById(R.id.one_line);
+   editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MaxLengthOfOneLineContent)});
+
     }
 }
