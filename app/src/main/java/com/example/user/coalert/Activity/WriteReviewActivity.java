54724c4516dbd078a13d26885275a682900cfb31
dokdokorea <7dpdud@naver.com>
2018-09-30 04:42:15 +0900

    delete initial info page and build review page

---
+++ app/src/main/java/com/example/user/coalert/Activity/WriteReviewActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.coalert.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.user.coalert.R;
+
+public class WriteReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.write_review);
+    }
+}
