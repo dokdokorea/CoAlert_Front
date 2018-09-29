2cc6a7c384ca52774275c72c1ec2a6f946543acb
Youngwoo <duddn14758@naver.com>
2018-09-30 01:05:40 +0900

    remove useless listview

--- app/src/main/java/com/example/user/coalert/Activity/CosmeticInformationActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/CosmeticInformationActivity.java
@@ -2,41 +2,31 @@ package com.example.user.coalert.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TabHost;
-import android.widget.TextView;
 
-import com.example.user.coalert.Adapter.CosmeticInformationAdapter.ingredientAdapter;
+import com.example.user.coalert.Adapter.TabIngredListAdapter.TabIngredientListAdapter;
 import com.example.user.coalert.R;
-import com.example.user.coalert.item.IngredientList;
-import com.example.user.coalert.item.SimpleReviewList;
-
-import org.w3c.dom.Text;
+import com.example.user.coalert.item.OneImgOneStringCardView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class CosmeticInformationActivity extends AppCompatActivity{
     TabHost tabHost1;
-    TextView tv;
+    RecyclerView ingredient;
+    ArrayList<OneImgOneStringCardView> IngArr;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cosmetic_information);
-        tv=findViewById(R.id.text);
 
         tabHost1=(TabHost)findViewById(R.id.tabHost1);
         tabHost1.setup();
 
         TabHost.TabSpec ts1=tabHost1.newTabSpec("Tab Spec 1");
-
-
-
         ts1.setContent(R.id.content1);
         ts1.setIndicator("성분");
         tabHost1.addTab(ts1);
@@ -51,12 +41,20 @@ public class CosmeticInformationActivity extends AppCompatActivity{
         ts3.setIndicator("Detail review");
         tabHost1.addTab(ts3);
 
-        tv.setText("HEllo");
-        ListView inglist=(ListView)findViewById(R.id.ingredient_list);
-        ArrayList<IngredientList> data=new ArrayList<>();
-        data.add(new IngredientList(3,"toxic1"));
-        data.add(new IngredientList(5.2,"toxic2"));
-        data.add(new IngredientList(4.3,"toxic3"));
-        inglist.setAdapter(new ingredientAdapter(this,R.layout.item_cosmetic_information_ingredients,data));
+        ingredient=(RecyclerView)findViewById(R.id.tab_ingredient_list_recyclerview);
+        ingredient.setHasFixedSize(true);
+        ingredient.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        });
+        IngArr=new ArrayList<>();
+        IngArr.add(new OneImgOneStringCardView(R.drawable.cardview1,"toxic1"));
+        IngArr.add(new OneImgOneStringCardView(R.drawable.cardview1,"toxic2"));
+        IngArr.add(new OneImgOneStringCardView(R.drawable.cardview1,"toxic3"));
+        IngArr.add(new OneImgOneStringCardView(R.drawable.cardview1,"toxic4"));
+        IngArr.add(new OneImgOneStringCardView(R.drawable.cardview1,"toxic5"));
+        ingredient.setAdapter(new TabIngredientListAdapter(IngArr));
     }
 }
