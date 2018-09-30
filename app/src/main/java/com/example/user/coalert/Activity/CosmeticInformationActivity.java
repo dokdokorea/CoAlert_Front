bd8f33755edae639370ce38f0939b1b51c08331a
Youngwoo <duddn14758@naver.com>
2018-10-01 02:54:37 +0900

    add information wish btn

--- app/src/main/java/com/example/user/coalert/Activity/CosmeticInformationActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/CosmeticInformationActivity.java
@@ -1,11 +1,16 @@
 package com.example.user.coalert.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.coalert.Adapter.CosmeticInformationAdapter.SimpleReviewAdapter;
 import com.example.user.coalert.Adapter.TabIngredListAdapter.TabIngredientListAdapter;
@@ -18,13 +23,50 @@ import java.util.ArrayList;
 public class CosmeticInformationActivity extends AppCompatActivity{
     TabHost tabHost1;
     RecyclerView ingredient, simple,detail;
+    int percent=50,j=0;
+    TextView matching;
+    ImageButton wishbtn;
     ArrayList<OneImgOneStringCardView> IngArr;
     ArrayList<OneImgOneStringOneNumberCardView> SimpleArr;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cosmetic_information);
+        setContentView(R.layout.activity_cosmetic_information);
+        wishbtn=(ImageButton)findViewById(R.id.wish_btn);
+        matching=(TextView)findViewById(R.id.matchingpercent);
+        matching.setText(percent+"%");
+        if(percent<20){
+            matching.setTextColor(Color.RED);
+        }else if(percent<40){
+            matching.setTextColor(Color.YELLOW);
+        }else if(percent<60){
+            matching.setTextColor(Color.BLUE);
+        }else
+            matching.setTextColor(Color.GREEN);
+
+        if (j == 0)
+            wishbtn.setImageResource(R.drawable.emptyheart);
+        else
+            wishbtn.setImageResource(R.drawable.fullheart);
+
+        wishbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                j = 1 - j;
+
+                if (j == 0) {
+                    wishbtn.setImageResource(R.drawable.emptyheart);
+                    Toast.makeText(CosmeticInformationActivity.this, "찜하기 취소", Toast.LENGTH_SHORT).show();
+                } else {
+                    wishbtn.setImageResource(R.drawable.fullheart);
+                    Toast.makeText(CosmeticInformationActivity.this, "찜!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
 
         tabHost1=(TabHost)findViewById(R.id.tabHost1);
         tabHost1.setup();
@@ -36,12 +78,12 @@ public class CosmeticInformationActivity extends AppCompatActivity{
 
         TabHost.TabSpec ts2=tabHost1.newTabSpec("Tab Spec 2");
         ts2.setContent(R.id.content2);
-        ts2.setIndicator("simple review");
+        ts2.setIndicator("한줄리뷰");
         tabHost1.addTab(ts2);
 
         TabHost.TabSpec ts3=tabHost1.newTabSpec("Tab Spec 3");
         ts3.setContent(R.id.content3);
-        ts3.setIndicator("Detail review");
+        ts3.setIndicator("자세한리뷰");
         tabHost1.addTab(ts3);
 
         ingredient=(RecyclerView)findViewById(R.id.tab_ingredient_list_recyclerview);
