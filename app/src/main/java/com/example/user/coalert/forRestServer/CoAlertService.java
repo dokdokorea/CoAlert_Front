37dfce51f50dcd5a1b929ca3734aea3572461b6a
dl57934@naver.com <dngmadl14>
2018-10-01 08:00:26 +0900

    making signinModel

--- app/src/main/java/com/example/user/coalert/forRestServer/CoAlertService.java
+++ app/src/main/java/com/example/user/coalert/forRestServer/CoAlertService.java
@@ -12,15 +12,17 @@ public interface CoAlertService {
     Call<loginModel> loginCall(
             @Query("id") String id,
             @Query("password") String password,
-            @Query("imei")String imei
+            @Query("imei") String imei
     );
+
     @POST("cosmetic")
-    Call<cosmeticModel>  cosmeicCall(
+    Call<cosmeticModel> cosmeicCall(
             @Query("COMPANY") String company,
             @Query("CNAME") String cname,
             @Query("INGR") String ingr,
             @Query("CATEGORY") String category
     );
+
     @POST("review")
     Call<ReviewModel> reviewCall(
             @Query("COMPANY") String company,
@@ -31,10 +33,24 @@ public interface CoAlertService {
             @Query("ONELINE") String oneline, //한줄리뷰
             @Query("IMEI") String imei
     );
+
     @GET("search")
     Call<searchModel> searchCall(
-        @Query("text")String text
+            @Query("text") String text
     );
+
+    @POST("signin")
+    Call<signInModel> signInCall(
+            @Query("id") String id,
+            @Query("password") String password,
+            @Query("name") String name,
+            @Query("email") String email,
+            @Query("type")String type,
+            @Query("birth")String birth,
+            @Query("sex")String sex,
+            @Query("access")String access
+    );
+
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://192.168.43.153:3000/")
             .addConverterFactory(GsonConverterFactory.create())
