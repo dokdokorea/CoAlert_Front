40ab68aa3d559267f959506216137548c305c178
dl57934 <dl57934@pukyong.ac.kr>
2018-10-02 07:04:53 +0900

    complete active Listview making

--- app/src/main/java/com/example/user/coalert/Activity/recommendCosmeticShow.java
+++ app/src/main/java/com/example/user/coalert/Activity/recommendCosmeticShow.java
@@ -1,13 +1,17 @@
 package com.example.user.coalert.Activity;
 
-import android.annotation.SuppressLint;
+import
+        android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.user.coalert.R;
+import com.example.user.coalert.Adapter.recommendCosmeticAdapter;
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 
@@ -28,6 +32,22 @@ public class recommendCosmeticShow extends AppCompatActivity {
         String Cname = getReceiveData.getStringExtra("cname");
         showRecommendTextView.setText("추천 받을 화장품: " + Cname);
         String recommendCosmetics = getReceiveData.getStringExtra("recommendData");
+        recommendCosmeticAdapter recommendCosmeticAdapter = new recommendCosmeticAdapter();
+        String[] beforeToJson = getDataAtJson(recommendCosmetics);
+        for (int i = 0; i< 10; i++){
+            String[] splitJson = beforeToJson[i].split(",");
+            String cosmeticName = splitJson[0].substring(4);
+            String estimate = splitJson[1].substring(10, splitJson[1].length()-1);
+            setData(recommendCosmeticAdapter, cosmeticName, estimate);
+        }
+    }
+    public void setData(recommendCosmeticAdapter adapter, String Cname, String estimate){
+        adapter.addItem(Cname, estimate);
+        ListView RecommendCosmetic = findViewById(R.id.showRecommendListView);
+        RecommendCosmetic.setAdapter(adapter);
+    }
+    public String[] getDataAtJson(String recommendCosmetics){
+
         recommendCosmetics = recommendCosmetics.substring(1, recommendCosmetics.length() - 1);
         int comma = 0;
         StringBuilder recommendCosmeticsBuilder = new StringBuilder(recommendCosmetics);
@@ -39,17 +59,7 @@ public class recommendCosmeticShow extends AppCompatActivity {
                 comma +=1;
             }
         }
-        JsonParser jsonParser = new JsonParser();
         String[] beforeToJson = recommendCosmeticsBuilder.toString().split("@");
-        for (int i = 0; i< 10; i++){
-            String[] splitJson = beforeToJson[i].split(",");
-            String cosmeticName = splitJson[0].substring(4);
-            String estimate = splitJson[1].substring(10, splitJson[1].length()-1);
-            Log.e("asdasd", cosmeticName+estimate);
-        }
-
-//        jsonParser.parse(recommendCosmetics);
-//        Log.e("1등", recommendCosmetics);111
-
+        return beforeToJson;
     }
 }
--- app/src/main/java/com/example/user/coalert/Adapter/recommendCosmeticAdapter.java
+++ app/src/main/java/com/example/user/coalert/Adapter/recommendCosmeticAdapter.java
@@ -1,18 +1,27 @@
 package com.example.user.coalert.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.user.coalert.R;
+import com.example.user.coalert.item.recommendCosmeticItem;
+
+import java.util.ArrayList;
 
 public class recommendCosmeticAdapter extends BaseAdapter {
+    ArrayList<recommendCosmeticItem> itemList = new ArrayList();
     @Override
     public int getCount() {
-        return 0;
+        return itemList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return itemList.get(i);
     }
 
     @Override
@@ -22,6 +31,22 @@ public class recommendCosmeticAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        Context context = viewGroup.getContext();
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.activity_recommend_cosmetic_show_item, viewGroup, false);
+        }
+        TextView Cname = view.findViewById(R.id.recommendCname);
+        TextView percent = view.findViewById(R.id.matchingPercent);
+        recommendCosmeticItem item = (recommendCosmeticItem) getItem(i);
+        Cname.setText(item.getCname());
+        percent.setText(String.valueOf(item.getPercent()));
+        return view;
+    }
+    public void addItem(String Cname, String estimate){
+        recommendCosmeticItem recommendCosmeticItem = new recommendCosmeticItem();
+        recommendCosmeticItem.setCname(Cname);
+        recommendCosmeticItem.setPercent(Float.parseFloat(estimate)*20);
+        itemList.add(recommendCosmeticItem);
     }
 }
