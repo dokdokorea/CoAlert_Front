2d9a133c7055915ecbe36124291c533cfdd6141e
dokdokorea <7dpdud@naver.com>
2018-10-02 17:50:16 +0900

    move spinner codes from EmailSignUp to CommonSignUp

--- app/src/main/java/com/example/user/coalert/Activity/CommonSignUpActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/CommonSignUpActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -81,6 +83,27 @@ public class CommonSignUpActivity extends AppCompatActivity{
 
         //callback = new SessionCallback();
         //Session.getCurrentSession().addCallback(callback);
+
+        //YearSpinner
+        Spinner yearSpinner=(Spinner)findViewById(R.id.spinner_year);
+        ArrayAdapter yearAdapter=ArrayAdapter.createFromResource(this,
+                R.array.date_year,android.R.layout.simple_spinner_item);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(yearAdapter);
+
+        //MonthSpinner
+        Spinner monthSpinner=(Spinner)findViewById(R.id.spinner_month);
+        ArrayAdapter monthAdapter=ArrayAdapter.createFromResource(this,
+                R.array.date_month,android.R.layout.simple_spinner_item);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(monthAdapter);
+
+        //DaySpinner
+        Spinner daySpinner=(Spinner)findViewById(R.id.spinner_day);
+        ArrayAdapter dayAdapter=ArrayAdapter.createFromResource(this,
+                R.array.date_day,android.R.layout.simple_spinner_item);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/example/user/coalert/Activity/EmailSignUpActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/EmailSignUpActivity.java
@@ -2,37 +2,15 @@ package com.example.user.coalert.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
 import com.example.user.coalert.R;
 
-public class EmailSignUpActivity extends AppCompatActivity{
+public class EmailSignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.email_sign_up);
 
-        //YearSpinner
-        Spinner yearSpinner=(Spinner)findViewById(R.id.spinner_year);
-        ArrayAdapter yearAdapter=ArrayAdapter.createFromResource(this,
-                R.array.date_year,android.R.layout.simple_spinner_item);
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        yearSpinner.setAdapter(yearAdapter);
 
-        //MonthSpinner
-        Spinner monthSpinner=(Spinner)findViewById(R.id.spinner_month);
-        ArrayAdapter monthAdapter=ArrayAdapter.createFromResource(this,
-                R.array.date_month,android.R.layout.simple_spinner_item);
-        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        monthSpinner.setAdapter(monthAdapter);
-
-        //DaySpinner
-        Spinner daySpinner=(Spinner)findViewById(R.id.spinner_day);
-        ArrayAdapter dayAdapter=ArrayAdapter.createFromResource(this,
-                R.array.date_day,android.R.layout.simple_spinner_item);
-        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        daySpinner.setAdapter(dayAdapter);
     }
 }
