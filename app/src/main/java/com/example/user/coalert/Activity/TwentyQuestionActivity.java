6e846e0a1363ad4c56ae60fe9ef5824d5ba4bfe3
Youngwoo <duddn14758@naver.com>
2018-10-03 18:43:42 +0900

    add TewntyQuest xml

---
+++ app/src/main/java/com/example/user/coalert/Activity/TwentyQuestionActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.coalert.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.user.coalert.R;
+
+public class TwentyQuestionActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twenty_question);
+    }
+}
