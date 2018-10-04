001751314174c7d99c029bd0dfcd179669cb689c
dl57934 <dl57934@pukyong.ac.kr>
2018-10-04 20:34:01 +0900

    remove error;

--- app/src/main/java/com/example/user/coalert/Activity/WriteReviewActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/WriteReviewActivity.java
@@ -135,7 +135,7 @@ public class WriteReviewActivity extends AppCompatActivity {
 
             }
         });
-
+        personalPicRecyclerview = (RecyclerView) findViewById(R.id.personal_prod_pic);
         personalPicRecyclerview.setHasFixedSize(true);
         personalPicRecyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerArr = new ArrayList<>();
