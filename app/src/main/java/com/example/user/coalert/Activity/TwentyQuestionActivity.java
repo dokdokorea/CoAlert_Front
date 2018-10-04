2eb44efe0a56a0933b7f5b24715e49aa8a6c35af
Youngwoo <duddn14758@naver.com>
2018-10-04 21:24:56 +0900

    question algorithm

--- app/src/main/java/com/example/user/coalert/Activity/TwentyQuestionActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/TwentyQuestionActivity.java
@@ -9,19 +9,20 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.user.coalert.R;
+import com.transitionseverywhere.TransitionManager;
 
 public class TwentyQuestionActivity extends AppCompatActivity {
-    TextView question;
+    TextView question, Answer;
     Button No, Soso, Yes;
     int prev = 0, answer;
     LinearLayout QuestionPage;
-    //final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.container);
+    ViewGroup transitionsContainer;
     String QList[] = {"모공이 크지 않다", "모공이 매우 작다", "모공이 매우 크다", "모공의 크기가 적당하다", "모공의 크기가 부분적으로 다르다"
             , "눈 입 주위에 잔주름이 많다", "겨울철은 당기고 여름철은 유분이 많다", "피부가 두껍고, 피지분비가 많다", "윤기가 없고 피부도 칙칙하다",
-    "얼굴이 자주 빨개진다","피부톤이 밝고 고른편이다", "환절기에 피부트러블이 생긴다","피부톤이 전체적으로 어둡다","세안 후 당기고 T존 부위는 유분이 많다",
-    "얼굴이 많이 건조하다","피부에 윤기가 없다","나이에 비해 피부가 좋다","화장이 잘 지워지고 유분이 많다","T존 부위만 화장이 잘 지워진다","피곤하면 트러블이 생긴다",
-    "세안 후 얼굴이 전체적으로 당긴다","화장이 오래 간다", "여드름성 트러블이 잘 일어난다", "얼굴이 부분적으로 번들거린다", "화장품이나 비누를 바꾸면 트러블이 생긴다"};
-    String ResultList[]={"aaaa","bbbb","cccc","dddd","eeee"};
+            "얼굴이 자주 빨개진다", "피부톤이 밝고 고른편이다", "환절기에 피부트러블이 생긴다", "피부톤이 전체적으로 어둡다", "세안 후 당기고 T존 부위는 유분이 많다",
+            "얼굴이 많이 건조하다", "피부에 윤기가 없다", "나이에 비해 피부가 좋다", "화장이 잘 지워지고 유분이 많다", "T존 부위만 화장이 잘 지워진다", "피곤하면 트러블이 생긴다",
+            "세안 후 얼굴이 전체적으로 당긴다", "화장이 오래 간다", "여드름성 트러블이 잘 일어난다", "얼굴이 부분적으로 번들거린다", "화장품이나 비누를 바꾸면 트러블이 생긴다"};
+    String ResultList[] = {"건성피부", "일반피부", "지성피부", "복합성피부", "민감성피부"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,9 @@ public class TwentyQuestionActivity extends AppCompatActivity {
         No = (Button) findViewById(R.id.nobtn);
         Soso = (Button) findViewById(R.id.sosobtn);
         Yes = (Button) findViewById(R.id.yesbtn);
-        QuestionPage=(LinearLayout)findViewById(R.id.question_page);
+        QuestionPage = (LinearLayout) findViewById(R.id.question_page);
+        transitionsContainer = (ViewGroup) findViewById(R.id.container);
+        Answer = (TextView) findViewById(R.id.skin_type_result);
 
         question.setText(QList[0]);
 
@@ -39,12 +42,11 @@ public class TwentyQuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 answer = 0;
-                prev=getQuestion(prev, answer);
+                prev = getQuestion(prev, answer);
 
-             if(prev>24){
+                setView(prev);
+                //setfinal();
 
-             }
-                question.setText(QList[prev]);
             }
         });
 
@@ -52,8 +54,9 @@ public class TwentyQuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 answer = 1;
-                prev=getQuestion(prev, answer);
-                question.setText(QList[prev]);
+                prev = getQuestion(prev, answer);
+
+                setView(prev);
             }
         });
 
@@ -61,18 +64,47 @@ public class TwentyQuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 answer = 2;
-                prev=getQuestion(prev, answer);
-                question.setText(QList[prev]);
+                prev = getQuestion(prev, answer);
+
+                setView(prev);
             }
         });
     }
+    private void setView(int result){
+
+        TransitionManager.beginDelayedTransition(transitionsContainer);
+        Soso.setVisibility(View.GONE);
+
+        if (result == 18 || result == 19) {
+            TransitionManager.beginDelayedTransition(transitionsContainer);
+            Soso.setVisibility(View.GONE);
+        } else {
+            Soso.setVisibility(View.VISIBLE);
+
+        }
+
+        if (result > 24) {
+            TransitionManager.beginDelayedTransition(transitionsContainer);
+            QuestionPage.setVisibility(View.INVISIBLE);
+            Answer.setVisibility(View.VISIBLE);
+            Answer.setText(ResultList[result-24]);
+        } else
+            question.setText(QList[prev]);
+
+    }
+
+    private void setfinal(){
+        TransitionManager.beginDelayedTransition(transitionsContainer);
+        QuestionPage.setVisibility(View.INVISIBLE);
+        Answer.setVisibility(View.VISIBLE);
+    }
 
     private int getQuestion(int prev, int answer) {
-        int next=0;
-        switch(prev){
+        int next = 0;
+        switch (prev) {
 
             case 0:
-                switch(answer) {
+                switch (answer) {
                     case 0:
                         next = 1;
                         break;
@@ -86,7 +118,7 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                 break;
 
             case 1:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 2;
                         break;
@@ -100,7 +132,7 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                 break;
 
             case 2:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 3;
                         break;
@@ -108,13 +140,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 8;
                         break;
                     case 2:
-                        next = 5;
+                        next = 7;
                         break;
                 }
                 break;
 
             case 3:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 4;
                         break;
@@ -122,13 +154,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 5;
                         break;
                     case 2:
-                        next = 5;
+                        next = 6;
                         break;
                 }
                 break;
 
             case 4:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 5;
                         break;
@@ -136,13 +168,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 8;
                         break;
                     case 2:
-                        next = 5;
+                        next = 9;
                         break;
                 }
                 break;
 
             case 5:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 6;
                         break;
@@ -150,13 +182,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 11;
                         break;
                     case 2:
-                        next = 5;
+                        next = 10;
                         break;
                 }
                 break;
 
             case 6:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 7;
                         break;
@@ -164,13 +196,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 12;
                         break;
                     case 2:
-                        next = 5;
+                        next = 11;
                         break;
                 }
                 break;
 
             case 7:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 8;
                         break;
@@ -178,13 +210,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 13;
                         break;
                     case 2:
-                        next = 5;
+                        next = 12;
                         break;
                 }
                 break;
 
             case 8:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 9;
                         break;
@@ -192,13 +224,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 14;
                         break;
                     case 2:
-                        next = 5;
+                        next = 13;
                         break;
                 }
                 break;
 
             case 9:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 10;
                         break;
@@ -206,13 +238,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 10;
                         break;
                     case 2:
-                        next = 5;
+                        next = 14;
                         break;
                 }
                 break;
 
             case 10:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 11;
                         break;
@@ -220,13 +252,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 17;
                         break;
                     case 2:
-                        next = 5;
+                        next = 15;
                         break;
                 }
                 break;
 
             case 11:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 12;
                         break;
@@ -234,13 +266,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 15;
                         break;
                     case 2:
-                        next = 5;
+                        next = 16;
                         break;
                 }
                 break;
 
             case 12:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 13;
                         break;
@@ -248,13 +280,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 16;
                         break;
                     case 2:
-                        next = 5;
+                        next = 17;
                         break;
                 }
                 break;
 
             case 13:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 14;
                         break;
@@ -262,13 +294,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 17;
                         break;
                     case 2:
-                        next = 5;
+                        next = 18;
                         break;
                 }
                 break;
 
             case 14:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 16;
                         break;
@@ -276,13 +308,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 18;
                         break;
                     case 2:
-                        next = 5;
+                        next = 19;
                         break;
                 }
                 break;
 
             case 15:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 16;
                         break;
@@ -290,13 +322,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 21;
                         break;
                     case 2:
-                        next = 5;
+                        next = 20;
                         break;
                 }
                 break;
 
             case 16:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 17;
                         break;
@@ -304,13 +336,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 23;
                         break;
                     case 2:
-                        next = 5;
+                        next = 21;
                         break;
                 }
                 break;
 
             case 17:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 18;
                         break;
@@ -318,13 +350,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 23;
                         break;
                     case 2:
-                        next = 5;
+                        next = 22;
                         break;
                 }
                 break;
 
             case 18:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 19;
                         break;
@@ -332,13 +364,13 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 7;
                         break;
                     case 2:
-                        next = 5;
+                        next = 23;
                         break;
                 }
                 break;
 
             case 19:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 10;
                         break;
@@ -346,77 +378,77 @@ public class TwentyQuestionActivity extends AppCompatActivity {
                         next = 7;
                         break;
                     case 2:
-                        next = 5;
+                        next = 24;
                         break;
                 }
                 break;
 
             case 20:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 21;
                         break;
                     case 1:
-                        next = 7;
+                        next = 26;
                         break;
                     case 2:
-                        next = 5;
+                        next = 25;
                         break;
                 }
                 break;
 
             case 21:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 22;
                         break;
                     case 1:
-                        next = 7;
+                        next = 25;
                         break;
                     case 2:
-                        next = 5;
+                        next = 26;
                         break;
                 }
                 break;
 
             case 22:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 23;
                         break;
                     case 1:
-                        next = 7;
+                        next = 28;
                         break;
                     case 2:
-                        next = 5;
+                        next = 27;
                         break;
                 }
                 break;
 
             case 23:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 24;
                         break;
                     case 1:
-                        next = 7;
+                        next = 27;
                         break;
                     case 2:
-                        next = 5;
+                        next = 28;
                         break;
                 }
                 break;
 
             case 24:
-                switch(answer){
+                switch (answer) {
                     case 0:
                         next = 17;
                         break;
                     case 1:
-                        next = 7;
+                        next = 28;
                         break;
                     case 2:
-                        next = 5;
+                        next = 29;
                         break;
                 }
                 break;
