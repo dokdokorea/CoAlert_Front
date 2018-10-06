b95048df98c6183b82a6244b982a36b11bd100fd
Youngwoo <duddn14758@naver.com>
2018-10-07 06:25:40 +0900

    activate cardview clickListener in fragment_home

--- app/src/main/java/com/example/user/coalert/Adapter/FragmentHomeElementAdapter/HotYoutuberAdapter.java
+++ app/src/main/java/com/example/user/coalert/Adapter/FragmentHomeElementAdapter/HotYoutuberAdapter.java
@@ -1,12 +1,15 @@
 package com.example.user.coalert.Adapter.FragmentHomeElementAdapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.coalert.R;
 import com.example.user.coalert.item.OneImgOneStringCardView;
@@ -15,20 +18,26 @@ import java.util.ArrayList;
 
 public class HotYoutuberAdapter extends RecyclerView.Adapter<HotYoutuberAdapter.ViewHolder> {
     private ArrayList<OneImgOneStringCardView> mDataSet;
+    Context context;
+    int item_layout;
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView textView;
+        private CardView cardView;
 
         ViewHolder(View itemView) {
             super(itemView);
             imageView = (ImageView) itemView.findViewById(R.id.youtube);
             textView = (TextView) itemView.findViewById(R.id.youtube_title);
+            cardView=itemView.findViewById(R.id.hot_youtuber_cardview);
         }
     }
 
-    public HotYoutuberAdapter(ArrayList<OneImgOneStringCardView> myDataset) {
+    public HotYoutuberAdapter(Context context,ArrayList<OneImgOneStringCardView> myDataset,int item_layout) {
         mDataSet = myDataset;
+        this.context=context;
+        this.item_layout=item_layout;
     }
 
     @NonNull
@@ -39,9 +48,16 @@ public class HotYoutuberAdapter extends RecyclerView.Adapter<HotYoutuberAdapter.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HotYoutuberAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull HotYoutuberAdapter.ViewHolder holder, final int position) {
         holder.imageView.setImageResource(mDataSet.get(position).getImage());
         holder.textView.setText(mDataSet.get(position).getText());
+        holder.cardView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,mDataSet.get(position).getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/user/coalert/Adapter/FragmentHomeElementAdapter/NewProductAdapter.java
+++ app/src/main/java/com/example/user/coalert/Adapter/FragmentHomeElementAdapter/NewProductAdapter.java
@@ -1,12 +1,15 @@
 package com.example.user.coalert.Adapter.FragmentHomeElementAdapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.coalert.R;
 import com.example.user.coalert.item.OneImgTwoStringCardView;
@@ -15,22 +18,28 @@ import java.util.ArrayList;
 
 public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.ViewHolder> {
     private ArrayList<OneImgTwoStringCardView> mDataset;
+    Context context;
+    int item_layout;
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView prodNameView;
         private TextView companyView;
+        private CardView cardView;
 
         ViewHolder(View itemView) {
             super(itemView);
             imageView = (ImageView) itemView.findViewById(R.id.new_product_image);
             prodNameView = (TextView) itemView.findViewById(R.id.new_product_name);
             companyView = (TextView) itemView.findViewById(R.id.company_name);
+            cardView=itemView.findViewById(R.id.new_product_cardview);
         }
     }
 
-    public NewProductAdapter(ArrayList<OneImgTwoStringCardView> myDataset) {
+    public NewProductAdapter(Context context,ArrayList<OneImgTwoStringCardView> myDataset,int item_layout) {
         mDataset = myDataset;
+        this.context=context;
+        this.item_layout=item_layout;
     }
 
     @NonNull
@@ -41,10 +50,16 @@ public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NewProductAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NewProductAdapter.ViewHolder holder, final int position) {
         holder.imageView.setImageResource(mDataset.get(position).getImage());
         holder.prodNameView.setText(mDataset.get(position).getText1());
         holder.companyView.setText(mDataset.get(position).getText2());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,mDataset.get(position).getText2(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
