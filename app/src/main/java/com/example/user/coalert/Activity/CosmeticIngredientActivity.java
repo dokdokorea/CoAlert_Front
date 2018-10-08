ddf50e38f85319ad55d4f57436fd5308c38fbe8f
yeyoung_desktop <7dpdud@naver.com>
2018-10-09 01:04:17 +0900

    edit cosinfo page

--- app/src/main/java/com/example/user/coalert/Activity/CosmeticIngredientActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/CosmeticIngredientActivity.java
@@ -7,9 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.widget.TabHost;
 
-import com.example.user.coalert.Adapter.TabIngredListAdapter.CombinationSkinAdapter;
 import com.example.user.coalert.Adapter.TabIngredListAdapter.DrySkinAdapter;
-import com.example.user.coalert.Adapter.TabIngredListAdapter.NormalSkinAdapter;
 import com.example.user.coalert.Adapter.TabIngredListAdapter.OilySkinAdapter;
 import com.example.user.coalert.Adapter.TabIngredListAdapter.SensitiveSkinAdapter;
 import com.example.user.coalert.Adapter.TabIngredListAdapter.TabIngredientListAdapter;
@@ -19,8 +17,8 @@ import com.example.user.coalert.item.OneImgOneStringCardView;
 import java.util.ArrayList;
 
 public class CosmeticIngredientActivity extends AppCompatActivity {
-    RecyclerView tabIngredientListRecyclerView, oilySkinRecyclerView, normalSkinRecyclerView, drySkinRecyclerView, combinationRecyclerView, sensitiveRecylcerView;
-    ArrayList<OneImgOneStringCardView> tabIngArr, oilArr, normalArr, dryArr, combinationArr, sensitiveArr;
+    RecyclerView tabIngredientListRecyclerView, oilySkinRecyclerView,drySkinRecyclerView, sensitiveRecyclerView;
+    ArrayList<OneImgOneStringCardView> tabIngArr, oilArr, dryArr, sensitiveArr;
     TabHost tabHost1;
 
     @Override
@@ -43,30 +41,22 @@ public class CosmeticIngredientActivity extends AppCompatActivity {
 
         tabIngredientListRecyclerView = (RecyclerView) findViewById(R.id.tab_ingredient_list_recyclerview);
         oilySkinRecyclerView = (RecyclerView) findViewById(R.id.oily_skin_recycler_view);
-        normalSkinRecyclerView = (RecyclerView) findViewById(R.id.normal_skin_recycler_view);
         drySkinRecyclerView = (RecyclerView) findViewById(R.id.dry_skin_recycler_view);
-        combinationRecyclerView = (RecyclerView) findViewById(R.id.combination_skin_recycler_view);
-        sensitiveRecylcerView = (RecyclerView) findViewById(R.id.sentitive_skin_recycler_view);
+        sensitiveRecyclerView = (RecyclerView) findViewById(R.id.sentitive_skin_recycler_view);
 
         tabIngredientListRecyclerView.setHasFixedSize(true);
         oilySkinRecyclerView.setHasFixedSize(true);
-        normalSkinRecyclerView.setHasFixedSize(true);
         drySkinRecyclerView.setHasFixedSize(true);
-        combinationRecyclerView.setHasFixedSize(true);
-        sensitiveRecylcerView.setHasFixedSize(true);
+        sensitiveRecyclerView.setHasFixedSize(true);
 
         tabIngredientListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         oilySkinRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        normalSkinRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         drySkinRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        combinationRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        sensitiveRecylcerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        sensitiveRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         tabIngArr = new ArrayList<>();
         oilArr = new ArrayList<>();
-        normalArr = new ArrayList<>();
         dryArr = new ArrayList<>();
-        combinationArr = new ArrayList<>();
         sensitiveArr = new ArrayList<>();
 
 
@@ -76,16 +66,10 @@ public class CosmeticIngredientActivity extends AppCompatActivity {
         oilArr.add(new OneImgOneStringCardView(R.drawable.cardview2, "당신에게 행복을"));
         oilySkinRecyclerView.setAdapter(new OilySkinAdapter(oilArr));
 
-        normalArr.add(new OneImgOneStringCardView(R.drawable.cardview2, "당신에게 행복을"));
-        normalSkinRecyclerView.setAdapter(new NormalSkinAdapter(normalArr));
-
         dryArr.add(new OneImgOneStringCardView(R.drawable.cardview2, "당신에게 행복을"));
         drySkinRecyclerView.setAdapter(new DrySkinAdapter(dryArr));
 
-        combinationArr.add(new OneImgOneStringCardView(R.drawable.cardview2, "당신에게 행복을"));
-        combinationRecyclerView.setAdapter(new CombinationSkinAdapter(combinationArr));
-
         sensitiveArr.add(new OneImgOneStringCardView(R.drawable.cardview2, "당신에게 행복을"));
-        sensitiveRecylcerView.setAdapter(new SensitiveSkinAdapter(sensitiveArr));
+        sensitiveRecyclerView.setAdapter(new SensitiveSkinAdapter(sensitiveArr));
     }
 }
