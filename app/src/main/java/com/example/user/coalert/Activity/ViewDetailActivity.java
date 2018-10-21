7f356803eccb0290b1a9a7d80da7399cb2c82f8f
Youngwoo <duddn14758@naver.com>
2018-10-21 10:22:17 +0900

    edit cosmetic info

--- app/src/main/java/com/example/user/coalert/Activity/ViewDetailActivity.java
+++ app/src/main/java/com/example/user/coalert/Activity/ViewDetailActivity.java
@@ -64,97 +64,7 @@ public class ViewDetailActivity extends AppCompatActivity {
     ImageButton selected_detail_backBtn;
     Button moreinfo;
 
-    String context = "I'm twenty three \n" +
-            "난 수수께끼 (Question)\n" +
-            "뭐게요 맞혀봐요\n" +
-            "I'm twenty three\n" +
-            "틀리지 말기 Because \n" +
-            "난 몹시 예민해요\n" +
-            "맞춰봐\n" +
-            "\n" +
-            "한 떨기 스물셋 좀 \n" +
-            "아가씨 태가 나네\n" +
-            "다 큰 척해도 적당히 믿어줘요\n" +
-            "\n" +
-            "얄미운 스물셋\n" +
-            "아직 한참 멀었다 얘 \n" +
-            "덜 자란 척해도\n" +
-            "대충 속아줘요\n" +
-            "\n" +
-            "난, 그래 확실히 지금이 좋아요 \n" +
-            "아냐, 아냐 사실은 때려 치고 싶어요\n" +
-            "아 알겠어요 난 사랑이 하고 싶어\n" +
-            "아니 돈이나 많이 벌래\n" +
-            "맞춰봐\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "얼굴만 보면 몰라\n" +
-            "속마음과 다른 표정을 짓는 일\n" +
-            "아주 간단하거든\n" +
-            "어느 쪽이게?\n" +
-            "사실은 나도 몰라\n" +
-            "애초에 나는 단 한 줄의 \n" +
-            "거짓말도 쓴 적이 없거든\n" +
-            "\n" +
-            "여우인 척, 하는 곰인 척, 하는 여우 아니면\n" +
-            "아예 다른 거\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "뭐든 한 쪽을 골라\n" +
-            "색안경 안에 비춰지는 거 뭐 이제 익숙하거든\n" +
-            "Check it out\n" +
-            "\n" +
-            "겁나는 게 없어요\n" +
-            "엉망으로 굴어도 \n" +
-            "사람들은 내게 매일 친절해요\n" +
-            "\n" +
-            "인사하는 저 여자\n" +
-            "모퉁이를 돌고도 아직 웃고 있을까\n" +
-            "늘 불안해요\n" +
-            "\n" +
-            "난, 영원히 아이로 남고 싶어요\n" +
-            "아니, 아니 물기 있는 여자가 될래요\n" +
-            "아 정했어요 난 죽은 듯이 살래요\n" +
-            "아냐, 다 뒤집어 볼래\n" +
-            "맞춰봐\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "얼굴만 보면 몰라\n" +
-            "속마음과 다른 표정을 짓는 일\n" +
-            "아주 간단하거든\n" +
-            "어느 쪽이게?\n" +
-            "사실은 나도 몰라\n" +
-            "애초에 나는 단 한 줄의 \n" +
-            "거짓말도 쓴 적이 없거든\n" +
-            "\n" +
-            "여우인 척, 하는 곰인 척, 하는 여우 아니면\n" +
-            "아예 다른 거\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "뭐든 한 쪽을 골라\n" +
-            "색안경 안에 비춰지는 거 뭐 이제 익숙하거든\n" +
-            "\n" +
-            "난 당신 맘에 들고 싶어요\n" +
-            "아주 살짝만 얄밉게 해도 돼요?\n" +
-            "난 당신 맘에 들고 싶어요\n" +
-            "자기 머리 꼭대기 위에서 놀아도 돼요?\n" +
-            "맞춰봐\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "얼굴만 보면 몰라\n" +
-            "속마음과 다른 표정을 짓는 일\n" +
-            "아주 간단하거든\n" +
-            "어느 쪽이게?\n" +
-            "사실은 나도 몰라\n" +
-            "애초에 나는 단 한 줄의 \n" +
-            "거짓말도 쓴 적이 없거든\n" +
-            "\n" +
-            "여우인 척, 하는 곰인 척, 하는 여우 아니면\n" +
-            "아예 다른 거\n" +
-            "\n" +
-            "어느 쪽이게?\n" +
-            "뭐든 한 쪽을 골라\n" +
-            "색안경 안에 비춰지는 거 뭐 이제 익숙하거든";
+    String context = "context";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -180,7 +90,6 @@ public class ViewDetailActivity extends AppCompatActivity {
         comment=(RecyclerView)findViewById(R.id.commentRecycler);
         GoodBtn=(TextView)findViewById(R.id.good_btn);
         WriteReveiw=(TextView)findViewById(R.id.writeComment);
-        GlobalApplication info=(GlobalApplication) getApplication();
 
 
         LikeCount.setText("3000");
@@ -188,7 +97,6 @@ public class ViewDetailActivity extends AppCompatActivity {
         CreatorId.setText("dlwlrma");
         PostTitle.setText("Twenty Three");
         PostContext.setText(context);
-        PostContext.setText(info.getId());
         count=Integer.parseInt(LikeCount.getText().toString());
         if (j == 0) {
             LikeIcon.setImageResource(R.drawable.black_empty_heart);
